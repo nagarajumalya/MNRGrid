48edb74f266cfd28daf3a68f5f2ef54ce3580d88
nagarajumalya <48435208+nagarajumalya@users.noreply.github.com>
2024-03-18 12:34:43 +0530

    Add files via upload

---
+++ MVCGridExample/Controllers/GridController.cs
@@ -0,0 +1,59 @@
+﻿using MVCGridExample.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using Newtonsoft.Json;
+
+namespace MVCGridExample.Controllers
+{
+    public class GridController : Controller
+    {
+        // GET: Grid
+        public ActionResult Index()
+        {
+            string jsonString = @"{
+            ""status"":""success"",
+            ""count"": 3,
+            ""metadata"":{
+            ""confidenceScore"": 0.8
+            },
+            ""results"":
+            [
+            {
+            ""id"": 1,
+            ""score"": 0.8,
+            ""path"": ""1.pdf""
+            },
+            {
+            ""id"": 2,
+            ""score"": 0.75,
+            ""path"": ""2.pdf""
+            },
+            {
+            ""id"": 3,
+            ""score"": 0.66,
+            ""path"": ""3.pdf""
+            }
+            ]}";
+            Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(jsonString);
+            ResultGridModelList glm = new ResultGridModelList();
+            List<ResultGridModel> gl = new List<ResultGridModel>();
+           
+            foreach (var item in myDeserializedClass.results)
+            {
+                ResultGridModel gm = new ResultGridModel();
+                gm.id = item.id;
+                gm.path = item.path;
+                gm.score = item.score;
+                gl.Add(gm);
+            }
+            glm.ResultGridData = gl;
+
+            ViewData["selectedResumeViewer"] = "active";
+            return View("~/Views/Grid/GridView.cshtml", glm);
+        }
+    }
+}
---
+++ MVCGridExample/Models/GridModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCGridExample.Models
+{
+    //public class GridModel
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+
+    //    public string Desc { get; set; }
+    //    public string Date { get; set; }
+    //}
+    //public class GridModelList
+    //{
+    //    public List<GridModel> GridData { get; set; }
+    //}
+    /// <summary>
+    /// New Model
+    /// </summary>
+    public class Metadata
+    {
+        public double confidenceScore { get; set; }
+    }
+
+    public class ResultGridModel
+    {
+        public int id { get; set; }
+        public double score { get; set; }
+        public string path { get; set; }
+    }
+    public class ResultGridModelList
+    {
+        public List<ResultGridModel> ResultGridData { get; set; }
+    }
+
+    public class Root
+    {
+        public string status { get; set; }
+        public int count { get; set; }
+        public Metadata metadata { get; set; }
+        public List<ResultGridModel> results { get; set; }
+    }
+}
